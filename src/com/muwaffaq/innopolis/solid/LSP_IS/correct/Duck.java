e6d2b0ec14b361a042ce3343301effad8bcc37de
Hamada Salhab <31417987+HamadaSalhab@users.noreply.github.com>
2022-03-18 15:56:11 +0300

    Create Duck.java

---
+++ src/com/muwaffaq/innopolis/solid/LSP_IS/correct/Duck.java
@@ -0,0 +1,24 @@
+package com.muwaffaq.innopolis.solid.LSP_IS.correct;
+
+public class Duck implements FlywingAndSwimmingBird {
+
+    @Override
+    public void fly() {
+        System.out.println("Flying...");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("eating");
+    }
+
+    @Override
+    public void hatch() {
+        System.out.println("laying eggs");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("swimming");
+    }
+}
